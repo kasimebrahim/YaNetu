356d775be89d2fb9d35cde804b38da9bc94bcd4a
IsraelA777 <se.israelabebe@gmail.com>
2016-12-07 16:21:57 +0300

    added player behavior to move automatically when started and some astatic modefication on the house

---
+++ Assets/playerBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerBehavior : MonoBehaviour {
+
+
+
+
+	public int max= 150;
+	public int speed = 7;
+	int i = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(i<max)
+		{
+			transform.Translate (Vector3.forward * Time.deltaTime * speed);
+			i++;
+		}
+	}
+
+
+
+	
+}
