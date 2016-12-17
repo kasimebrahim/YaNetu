5d1d260b60367eaac7ab2223d146c35e7d8a0217
israel7777 <se.israel.abebe777@gmail.com>
2016-12-17 09:26:54 -0800

    removed unwanted parts and modefied the move scripts

--- Assets/playerBehavior.cs
+++ Assets/playerBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class playerBehavior : MonoBehaviour {
 
 
@@ -8,22 +9,53 @@ public class playerBehavior : MonoBehaviour {
 
 	public int max= 150;
 	public int speed = 7;
-	int i = 0;
+	float i = 0;
+	public GameObject door;
+	private bool walk = true;
+	private bool startTeching = false;
 
 	// Use this for initialization
 	void Start () {
-		
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if(i<max)
+		RaycastHit hit;
+
+		if (Physics.Raycast(transform.position, Vector3.forward, out hit)){
+			print("Found an object - distance: " + hit.distance);
+			i = hit.distance;
+		
+		}
+		// use onother way to move becouse its sooo lame 
+		if(i>1 && walk==true)
 		{
 			transform.Translate (Vector3.forward * Time.deltaTime * speed);
-			i++;
+			//print ("walking");
+		}else{
+			walk = false;
+			startTeching = true;
+			door.transform.Translate(Vector3.forward * Time.deltaTime * speed);
 		}
+
+
+		if (startTeching == true) 
+		{
+			teach ();
+		}
+
+
+
 	}
+
+	void teach()
+	{
+	}
+
+
 
 
 
