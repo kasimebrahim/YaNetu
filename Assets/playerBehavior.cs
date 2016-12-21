3eb0d3a29191b336d77c1a5bff9cc54f47b77ef2
israel7777 <se.israel.abebe777@gmail.com>
2016-12-21 09:20:41 -0800

    some modefication

--- Assets/playerBehavior.cs
+++ Assets/playerBehavior.cs
@@ -5,17 +5,17 @@ using System.Collections;
 public class playerBehavior : MonoBehaviour {
 
 
-
-
 	public int max= 150;
 	public int speed = 7;
 	float i = 0;
 	public GameObject door;
 	private bool walk = true;
-	private bool startTeching = false;
+	private bool startedTeaching = false;
+
 
 	// Use this for initialization
 	void Start () {
+		
 
 
 	}
@@ -26,26 +26,24 @@ public class playerBehavior : MonoBehaviour {
 		RaycastHit hit;
 
 		if (Physics.Raycast(transform.position, Vector3.forward, out hit)){
-			print("Found an object - distance: " + hit.distance);
+			//print("Found an object - distance: " + hit.distance);
 			i = hit.distance;
-		
 		}
+
 		// use onother way to move becouse its sooo lame 
-		if(i>1 && walk==true)
-		{
+		if(i>1 && walk==true){
 			transform.Translate (Vector3.forward * Time.deltaTime * speed);
-			//print ("walking");
 		}else{
 			walk = false;
-			startTeching = true;
 			door.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+			//door.SetActive(false);
+			if (startedTeaching == false) {
+				teach ();
+			}
 		}
 
 
-		if (startTeching == true) 
-		{
-			teach ();
-		}
+
 
 
 
@@ -53,11 +51,9 @@ public class playerBehavior : MonoBehaviour {
 
 	void teach()
 	{
+		startedTeaching = true;
+		print("hello student");
+		print("this is yanetu world i will be your teacher in this class we will learn about numbers");
+
 	}
-
-
-
-
-
-	
 }
