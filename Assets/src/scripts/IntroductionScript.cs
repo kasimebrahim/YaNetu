6bd1683170b18cfff6ee42de8389d13fc448bcb5
solomon molla <solomon molla>
2016-12-31 11:50:33 -0800

    Dialogue system added

---
+++ Assets/src/scripts/IntroductionScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.src.dialogue;
+public class IntroductionScript : MonoBehaviour {
+    private DialogueSystem dialogue;
+    private ArrayList introductionScripts;
+	void Start () {
+        dialogue = new DialogueSystem();
+        introductionScripts = dialogue.getIntroductionScripts();
+        for (int i = 0; i < introductionScripts.Count; i++)
+        {
+            Debuge.Log(introductionScripts[i]);
+        }
+	}
+
+    void Update () {
+	
+	}
+}
