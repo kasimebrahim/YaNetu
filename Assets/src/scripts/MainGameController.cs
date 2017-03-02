c06e5155e171c30a3555fb4fcb7564ce98253ce0
solomon molla <solomon molla>
2017-03-02 09:11:03 -0800

    monkey sound added

--- Assets/src/scripts/MainGameController.cs
+++ Assets/src/scripts/MainGameController.cs
@@ -13,19 +13,33 @@ public class MainGameController : MonoBehaviour {
 	public GameObject puzzleBldg2;
 	public GameObject puzzleBldg3;
 	public GameObject puzzleBldg4;
+	AudioSource puzzle3Audio;
+	AudioSource puzzle2Audio;
+	AudioSource puzzle4Audio;
 	int puzzleIndex = 1;
 	int answer = 0;
+	AudioSource sound;
 	int currentCorrectAnswer = 3;
+	bool isIncrement = false;
+	bool isIncrement2 = false;
+	bool isIncrement3 = false;
 	void Start () {
 		index = 0;
 		targetPoint = pathParent.GetChild (index);
 		learnButton.gameObject.SetActive(false);
 		exitButton.gameObject.SetActive (false);
 		statusText.gameObject.SetActive (false);
+		sound = GetComponent<AudioSource> ();
+		puzzle2Audio = puzzleBldg2.GetComponent<AudioSource> ();
+		puzzle2Audio.Stop ();
+		puzzle3Audio = puzzleBldg3.GetComponent<AudioSource> ();
+		puzzle3Audio.Stop ();
+		puzzle4Audio = puzzleBldg4.GetComponent<AudioSource> ();
+		puzzle4Audio.Stop ();
 	}
 	
 	void Update () {
-		
+		//Debug.Log (puzzleIndex);
 		if (puzzleIndex == 1) {
 			if (answer == currentCorrectAnswer) {
 				GameObject monkey1 = puzzleBldg1.transform.GetChild (0).gameObject;
@@ -36,36 +50,53 @@ public class MainGameController : MonoBehaviour {
 				monkey3.GetComponent<Animator> ().Play ("long_Jump");
 
 				//answer = 0;
-				Debug.Log ("Correct");
+				//Debug.Log ("Correct");
 			} if (Vector3.Distance (transform.position, puzzleBldg1.transform.position) < 1f & (answer != currentCorrectAnswer)) {
 				speed = 0f;
+				sound.Stop ();
 				learnButton.gameObject.SetActive(true);
 				exitButton.gameObject.SetActive (true);
 				statusText.gameObject.SetActive (true);
-				Debug.Log ("You miss");
+				//Debug.Log ("You miss");
 			}
 			else if(answer ==1 || answer == 2 || answer == 4) {
-				Debug.Log ("Incorrect");
+				//Debug.Log ("Incorrect");
 			}
 		}
 		else if (puzzleIndex == 2) {
+			
+			if (Vector3.Distance (transform.position, puzzleBldg2.transform.position) <= 10) {
+				if (!isIncrement) {
+					puzzle2Audio.Play ();
+					isIncrement = true;
+				}
+
+			}
 			if (answer == currentCorrectAnswer) {
 				GameObject monkey1 = puzzleBldg2.transform.GetChild (0).gameObject;
 				monkey1.GetComponent<Animator> ().Play ("long_Jump");
-				Debug.Log ("Correct");
+				//Debug.Log ("Correct");
 			} if (Vector3.Distance (transform.position, puzzleBldg2.transform.position) < 1f & (answer == 0 || answer != currentCorrectAnswer)) {
 				speed = 0f;
+				sound.Stop ();
 				learnButton.gameObject.SetActive(true);
 				exitButton.gameObject.SetActive (true);
 				statusText.gameObject.SetActive (true);
-				Debug.Log ("You miss");
+				//Debug.Log ("You miss");
 			}
 			else if(answer ==3 || answer == 2 || answer == 4) {
-				Debug.Log ("Incorrect");
+				//Debug.Log ("Incorrect");
 			}
 		}
 		else if (puzzleIndex == 3) {
-			Debug.Log ("this is the point");
+			if (Vector3.Distance (transform.position, puzzleBldg3.transform.position) <= 10) {
+				if (!isIncrement2) {
+					puzzle3Audio.Play ();
+					isIncrement2 = true;
+				}
+
+			}
+			//Debug.Log ("this is the point");
 			if (answer == currentCorrectAnswer) {
 				GameObject monkey1 = puzzleBldg3.transform.GetChild (0).gameObject;
 				GameObject monkey2 = puzzleBldg3.transform.GetChild (1).gameObject;
@@ -76,19 +107,26 @@ public class MainGameController : MonoBehaviour {
 				monkey3.GetComponent<Animator> ().Play ("long_Jump");
 				monkey4.GetComponent<Animator> ().Play ("long_Jump");
 				//answer = 0;
-				Debug.Log ("Correct");
+				//Debug.Log ("Correct");
 			} if (Vector3.Distance (transform.position, puzzleBldg3.transform.position) < 1f & (answer != currentCorrectAnswer)) {
 				speed = 0f;
+				sound.Stop ();
 				learnButton.gameObject.SetActive(true);
 				exitButton.gameObject.SetActive (true);
 				statusText.gameObject.SetActive (true);
-				Debug.Log ("You miss");
+				//Debug.Log ("You miss");
 			}
 			else if(answer ==1 || answer == 2 || answer == 3) {
-				Debug.Log ("Incorrect");
+				//Debug.Log ("Incorrect");
 			}
 		}
 		else if (puzzleIndex == 4) {
+			if (Vector3.Distance (transform.position, puzzleBldg4.transform.position) <= 10) {
+				if (!isIncrement3) {
+					puzzle4Audio.Play ();
+					isIncrement3 = true;
+				}
+			}
 			Debug.Log ("this is the point");
 			if (answer == currentCorrectAnswer) {
 				GameObject monkey1 = puzzleBldg4.transform.GetChild (0).gameObject;
@@ -96,40 +134,48 @@ public class MainGameController : MonoBehaviour {
 				monkey1.GetComponent<Animator> ().Play ("long_Jump");
 				monkey2.GetComponent<Animator> ().Play ("long_Jump");
 
-				Debug.Log ("Correct");
+				//Debug.Log ("Correct");
 			} if (Vector3.Distance (transform.position, puzzleBldg4.transform.position) < 1f & (answer != currentCorrectAnswer)) {
 				speed = 0f;
+				sound.Stop ();
 				learnButton.gameObject.SetActive(true);
 				exitButton.gameObject.SetActive (true);
 				statusText.gameObject.SetActive (true);
-				Debug.Log ("You miss");
+				//Debug.Log ("You miss");
 			}
 			else if(answer ==1 || answer == 2 || answer == 3) {
-				Debug.Log ("Incorrect");
+				//Debug.Log ("Incorrect");
 			}
 		}
 		if (puzzleIndex == 1) {
 			if (Vector3.Distance (transform.position, puzzleBldg1.transform.position) > 0 && Vector3.Distance (transform.position, puzzleBldg1.transform.position) < 1) {
 				puzzleIndex++;
 				currentCorrectAnswer = 1;
+				//puzzle2Audio.Play ();
 			};
 		} 
 		else if (puzzleIndex == 2) {
 			if (Vector3.Distance (transform.position, puzzleBldg2.transform.position) > 0 && Vector3.Distance (transform.position, puzzleBldg2.transform.position) < 1) {
 				puzzleIndex++;
 				currentCorrectAnswer = 4;
+
 			};
+
 		} 
 		else if (puzzleIndex == 3) {
+			
 			if (Vector3.Distance (transform.position, puzzleBldg3.transform.position) > 0 && Vector3.Distance (transform.position, puzzleBldg3.transform.position) < 1) {
 				puzzleIndex++;
 				currentCorrectAnswer = 2;
+				//puzzle4Audio.Play ();
 			};
 		}
 		else if (puzzleIndex == 4) {
+			
 			if (Vector3.Distance (transform.position, puzzleBldg4.transform.position) > 0 && Vector3.Distance (transform.position, puzzleBldg4.transform.position) < 1) {
 				puzzleIndex++;
 				currentCorrectAnswer = 2;
+
 			};
 		} 
 
@@ -168,6 +214,11 @@ public class MainGameController : MonoBehaviour {
 				}
 
 
+			}
+			if (index == pathParent.childCount) {
+				Debug.Log ("shit");
+				sound.Stop ();
+				puzzle4Audio.Stop ();
 			}
 			//index %= pathParent.childCount;
 
