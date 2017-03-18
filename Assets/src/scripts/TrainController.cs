cfb23c28eaa00c799d3868339163edd4c7d81adb
solomon molla <solomon molla>
2017-03-17 22:04:35 -0700

    train controller script optimized

---
+++ Assets/src/scripts/TrainController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Assets.src.states.interfaces;
+using Assets.src.states;
+public class TrainController : MonoBehaviour {
+	int index;
+	public Transform pathParent;
+	public float Speed = 3f;
+	Transform targetPoint;
+	void Start () {
+		index = 0;
+		targetPoint = pathParent.GetChild (index);
+	}
+	void Update () {
+	
+	}
+	void FixedUpdate(){
+		transform.position = Vector3.MoveTowards (transform.position, targetPoint.position, Speed * Time.deltaTime);
+		Quaternion rot = targetPoint.rotation;
+		transform.LookAt(targetPoint);
+		if (Vector3.Distance(transform.position, targetPoint.position) < 0.1f){
+			index++;
+			if (index < pathParent.childCount) {
+				if (index > 2000) {
+					Speed = 0;
+				} else {
+					Speed = 3f;
+					targetPoint = pathParent.GetChild (index);
+				}					
+			}
+		}
+	}
+}
