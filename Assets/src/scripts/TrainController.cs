3aac60d8caec89e4b45b74e039d6acfc37e83081
solomon molla <solomon molla>
2017-04-06 03:06:38 -0700

    landscape changed

--- Assets/src/scripts/TrainController.cs
+++ Assets/src/scripts/TrainController.cs
@@ -8,7 +8,7 @@ using Assets.src.states;
 public class TrainController : MonoBehaviour {
 	int index;
 	public Transform pathParent;
-	public static float Speed = 3f;
+	public static float Speed = 2.5f;
 	Transform targetPoint;
 	void Start () {
 		index = 0;
@@ -25,12 +25,12 @@ public class TrainController : MonoBehaviour {
 		if (Vector3.Distance(transform.position, targetPoint.position) < 0.1f){
 			index++;
 			if (index < pathParent.childCount) {
-				if (index > 1000) {
-					Speed = 0;
-				} else {
-					Speed = 3f;
+					Speed = 2.5f;
 					targetPoint = pathParent.GetChild (index);
-				}					
+
+			}
+			if (index == pathParent.childCount) {
+				Speed = 0f;
 			}
 		}
 	}
