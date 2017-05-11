d873a3cb6e9fbd19643f9c25a00fa05d0108885b
israel7777 <se.israel.abebe777@gmail.com>
2017-05-11 15:29:20 +0200

    modified

---
+++ Assets/scripts/swipeTrail.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class swipeTrail : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (((Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved)
+			|| Input.GetMouseButton (0))) 
+		{
+
+			Plane objPlane = new Plane (Camera.main.transform.forward * -1, this.transform.position);
+
+			Ray mRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+			float rayDistance;
+			if (objPlane.Raycast (mRay, out rayDistance)) {
+				this.transform.position = mRay.GetPoint (rayDistance);
+			}
+		}
+	}
+}
--- Assets/src/states/StateLearn.cs
+++ Assets/src/states/StateLearn.cs
@@ -28,7 +28,7 @@ namespace Assets.src.states
 		public void artimetic(){
 			if (currentState == States.LEARN) {
 				YanetuController.Animate ("EvaAnimation2");
-				TeachingManager.teachArtimetic(6, 2,"/");
+				TeachingManager.teachArtimetic(4, 2,"/");
 				YanetuController.Speak ("Sound/1");
 				currentState = States.DETAIL;
 			} else if (currentState == States.DETAIL) {
